43b38492f74f2d5461dc3314e673358aec235853
Jesus Emanuel De Olivera <jesusdeolivera10@gmail.com>
2026-04-05 20:07:49 -0300

    camit: 1

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',        // ← ESTO genera archivos estáticos (HTML)
-  distDir: 'dist',         // ← Carpeta donde quedarán los archivos
   images: {
-    unoptimized: true,     // ← Necesario para export estático
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'daipvxjkxfxfsmwsoxfr.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
-  // Si tienes rutas dinámicas como /propiedad/[id], añade esto:
-  trailingSlash: true,     // ← Genera carpetas con index.html
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
